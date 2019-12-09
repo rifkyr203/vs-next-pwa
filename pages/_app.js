2163cfdce51908118ac89cb473b359a2382c6ae1
Rifky <rifky.rangkuti@gmail.com>
2019-12-09 23:50:25 +0700

    Feat: Manifest

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
-import React from 'react'
 import { Provider } from 'react-redux'
 import withRedux from '../store'
-
+import Head from 'next/head'
 
 const pwa = (props) => {
     const { Component, pageProps, store } = props
     return (
         <Provider store={store}>
+          <Head>
+            <meta charset="utf-8">
+            <title>Virtual Spirit</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta name="mobile-web-app-capable" content="yes">
+            <meta name="theme-color" content="#19A2B8">
+            <link rel="icon" href="/public/icons/favicon.ico" type="image/x-icon">
+            <link rel="manifest" href="manifest.json">
+          </Head>
           <Component {...pageProps} />
         </Provider>
       )
